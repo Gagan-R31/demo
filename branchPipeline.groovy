ba8269efa69a2d9679a777fd07f0665dbb6971f7
Gagan-R31 <167845584+Gagan-R31@users.noreply.github.com>
2024-12-02 15:26:30 +0530

    Create branchPipeline.groovy

---
+++ branchPipeline.groovy
@@ -0,0 +1,81 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'k8s-agent'
+            yaml """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              labels:
+                some-label: some-label-value
+            spec:
+              serviceAccountName: jenkins
+              containers:
+              - name: jnlp
+                image: jenkins/inbound-agent
+                args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
+              - name: kaniko
+                image: gaganr31/kaniko-go
+                command:
+                - /busybox/sh
+                tty: true
+                volumeMounts:
+                - name: kaniko-secret
+                  mountPath: /kaniko/.docker
+                - name: workspace-volume
+                  mountPath: /workspace
+              - name: kubectl
+                image: boxboat/kubectl
+                command:
+                - cat
+                tty: true
+              volumes:
+              - name: kaniko-secret
+                secret:
+                  secretName: kaniko-secret
+                  items:
+                  - key: .dockerconfigjson
+                    path: config.json
+              - name: workspace-volume
+                emptyDir: {}
+            """
+        }
+    }
+    environment {
+        GITHUB_TOKEN = credentials('github-token-gagan')
+        DOCKERHUB_REPO = 'gaganr31/argu'
+        REPO_URL = 'https://github.com/Gagan-R31/demo.git'
+        DEPLOYMENT_NAME = 'argu'
+        NAMESPACE = 'jenkins-operator'
+        COMMIT_SHA = ''
+        IMAGE_TAG = ''
+    }
+    stages {
+        stage('Clone Repository') {
+            steps {
+                script {
+                    def workspaceDir = pwd()
+                    sh """
+                    git clone -b version-1.0.1 https://${GITHUB_TOKEN}@github.com/Gagan-R31/demo.git
+                    """
+                    COMMIT_SHA = sh(script: "git -C ${workspaceDir}/demo rev-parse --short HEAD", returnStdout: true).trim()
+                    IMAGE_TAG = env.BRANCH_NAME  // Use the branch name as the Docker tag
+                }
+            }
+        }
+        stage('Build Docker Image with Kaniko') {
+            steps {
+                container('kaniko') {
+                    script {
+                        sh """
+                        cd demo
+                        /kaniko/executor --dockerfile=./Dockerfile \
+                                         --context=. \
+                                         --destination=${DOCKERHUB_REPO}:${IMAGE_TAG}
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
